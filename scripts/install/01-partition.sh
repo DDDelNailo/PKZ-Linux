e1d72970abc7875afbc69407a084acf38b33a7c3
DDDelNailo <danilodantas7002@gmail.com>
2026-06-13 12:09:23 -0300

    fix: ux formatting

--- scripts/install/01-partition.sh
+++ scripts/install/01-partition.sh
@@ -11,33 +11,27 @@ load_config
 
 step "Partitioning Disk"
 
-echo
-info "Configured disk: $DISK"
-echo
-
 info "Detected:"
 list_disks
 
 echo
+info "Configured disk: $DISK"
 
+echo
 if ! confirm_action "Install to $DISK?"; then
     warn "Operation cancelled."
     exit 1
 fi
 
 info "Creating GPT partition table"
-
 parted -s "$DISK" mklabel gpt
 
 info "Creating EFI partition"
-
 parted -s "$DISK" mkpart ESP fat32 1MiB "$EFI_SIZE"
 parted -s "$DISK" set 1 esp on
 
 info "Creating root partition"
-
 parted -s "$DISK" mkpart primary btrfs "$EFI_SIZE" 100%
-
 partprobe "$DISK"
 
 success "Partitioning completed"
